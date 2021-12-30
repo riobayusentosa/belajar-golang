104f245e018dfaef9053466d6a5f0cb4a173bfdc
unknown <riobayusentosa@gmail.com>
2021-12-30 14:52:08 +0700

    variable

---
+++ variable.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Rio Bayu"
+	fmt.Println(name)
+
+	name = "Rio Bayu Sentosa"
+	fmt.Println(name)
+
+	var friendname = "Budi"
+	fmt.Println(friendname)
+
+	var age int8 = 30
+	fmt.Println(age)
+
+	vname := "Aqmar"
+	fmt.Println(vname)
+
+	var (
+		firstname = "Muhammad"
+		lastname  = "Aqmar Zyan"
+	)
+	fmt.Println(firstname)
+	fmt.Println(lastname)
+}
