679bcaa4753daad51ed223393d223dbc4647f7bd
unknown <riobayusentosa@gmail.com>
2021-12-30 14:07:00 +0700

    string

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Rio")
+	fmt.Println("Rio Bayu")
+	fmt.Println("Rio Bayu Sentosa")
+
+	fmt.Println(len("Rio"))
+	fmt.Println("Rio Bayu"[0])
+	fmt.Println("Rio Bayu Sentosa"[1])
+}
