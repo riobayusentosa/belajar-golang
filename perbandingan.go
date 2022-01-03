f8c37ecb332eba0d3efd00dc0ed074b3dac8ad24
unknown <riobayusentosa@gmail.com>
2022-01-03 13:14:55 +0700

    perbandingan

---
+++ perbandingan.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name1 = "Rio"
+	var name2 = "rio"
+
+	var result bool = name1 == name2
+	fmt.Println(result)
+
+	var value1 = 100
+	var value2 = 200
+	fmt.Println(value1 > value2)
+	fmt.Println(value1 < value2)
+	fmt.Println(value1 == value2)
+	fmt.Println(value1 != value2)
+}
