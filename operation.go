a7d7db114be2640302ed493ea34d7208ef32a120
unknown <riobayusentosa@gmail.com>
2022-01-03 10:52:14 +0700

    operasi matematika

---
+++ operation.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = 10
+	var b = 10
+	var c = a + b
+
+	fmt.Println(c)
+
+	var i = 10
+	i += 10
+	fmt.Println(i)
+
+	i++
+	fmt.Println(i)
+
+	var negative = -200
+	var positif = +200
+	fmt.Println(negative)
+	fmt.Println(positif)
+
+	var j = 1
+	j++
+	j++
+	fmt.Println(j)
+}
